0f88a44c240c52080979f65016829011b03bf9ca
stephen shadrach <stephenlgz@gmail.com>
2022-09-20 09:40:50 -0400

    created list and article routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import React from "react";
+import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import "./App.css";
+import Article from "./pages/article";
+import List from "./pages/list";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <List />,
+  },
+  {
+    path: "/article",
+    element: <Article />,
+  }
+]);
 
 function App() {
   return (
     <div className="App">
-      <header></header>
+      <RouterProvider router={router} />
     </div>
   );
 }
