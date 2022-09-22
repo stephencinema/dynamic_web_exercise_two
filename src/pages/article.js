42b0d1ad589e5f7385ccf8d5e6ebd82601e63814
stephen shadrach <stephenlgz@gmail.com>
2022-09-22 10:33:15 -0400

    article list + article pages setup

--- src/pages/article.js
+++ src/pages/article.js
@@ -1,10 +1,20 @@
 import React from "react";
 
 function Article() {
+  const backgroundImageUrl = "url(https://htmlcolorcodes.com/assets/images/colors/gray-color-solid-background-1920x1080.png)";
+  const headerStyle = { backgroundImage: backgroundImageUrl, padding: "40px 25px" };
+
   return (
-    <div>
-      <h1>Article</h1>
-    </div>
+    <main className="article">
+      <header className="article--header" style={headerStyle}>
+        <div className="article--header--wrapper">
+          <h1>Article Title</h1>
+          <p>Wednesday, August 22</p>
+          <p className="blurb">testing testing testing</p>
+        </div>
+      </header>
+      <section className="article--content">{/* SECTION */}</section>
+    </main>
   );
 }
 
--- src/pages/list.js
+++ src/pages/list.js
@@ -1,10 +1,14 @@
 import React from "react";
+import ArticleCard from "../components/ArticleCard";
 
 function List() {
   return (
-    <div>
+    <main className="pageWrapper">
       <h1>Articles</h1>
-    </div>
+      <ArticleCard blurb="testing testing testing" date="article date" imageAlt="" imageSrc="" title="Article Title" />
+      <ArticleCard blurb="testing testing testing" date="article date" imageAlt="" imageSrc="" title="Article Title" />
+      <ArticleCard blurb="testing testing testing" date="article date" imageAlt="" imageSrc="" title="Article Title" />
+    </main>
   );
 }
 
